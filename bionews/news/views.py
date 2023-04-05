a7e456822d0c8e1ff3a4ff8c1f5a8f2eed413ce7
Kwanky-Millet <anuroopkeshavdraft.com>
2023-04-05 22:41:23 +0530

    change view

--- bionews/news/views.py
+++ bionews/news/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-import requests 
+import requests
 from .models import NewsArticle
 
 def index(request):
-    url = ('http://newsapi.org/v2/top-headlines?'
-           'country=us&category=science&'
-           'apiKey=9fce8f78bb684c3380c5d8e067a95899'
-    )
+    api_key = 'YOUR_API_KEY'
+    url = f'https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey={api_key}&q=biotechnology'
     response = requests.get(url)
     articles = response.json()['articles']
-
 
     for article in articles:
         title = article['title']
